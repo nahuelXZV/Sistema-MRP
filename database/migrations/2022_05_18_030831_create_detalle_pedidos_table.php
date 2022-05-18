0bbd430d82475928d0c243f2532252fd8759da77
Darwin Mamani <darwin_xd10@hotmail.com>
2022-05-17 23:15:49 -0400

    darwin, finalizo migrations

---
+++ database/migrations/2022_05_18_030831_create_detalle_pedidos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetallePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pedido_id')->nullable();
+            $table->unsignedBigInteger('producto_id')->nullable();
+            $table->unsignedBigInteger('estado_pedido_id')->nullable();
+            $table->unsignedInteger('cantidad')->nullable();
+            $table->foreign('pedido_id')->references('id')->on('pedidos')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('estado_pedido_id')->references('id')->on('estado_pedidos')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalle_pedidos');
+    }
+}
