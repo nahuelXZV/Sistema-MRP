5224dde05f78175913a0fb96ff0c6aa1e75ced6d
Darwin Mamani <darwin_xd10@hotmail.com>
2022-05-23 12:04:02 -0400

    darwin, unidades

--- app/Http/Livewire/Configuracion/SistemaUnidad/LwIndex.php
+++ app/Http/Livewire/Configuracion/SistemaUnidad/LwIndex.php
@@ -14,8 +14,8 @@ class LwIndex extends Component
 
     public $pagination = 10;
     public $attribute = '';
-    public $type = 'nombre';
-    public $sort = 'nombre';
+    public $type = 'id';
+    public $sort = 'id';
     public $direction = 'asc';
 
     public function updatingAttribute()
@@ -23,11 +23,29 @@ class LwIndex extends Component
         $this->resetPage();
     }
 
+    //Metodo de ordenado
+    public function order($type)
+    {
+        if ($this->type == $type) {
+            $this->direction = ($this->direction == 'desc')? ('asc') : ('desc');
+        } else {
+            $this->type = $type;
+            $this->direction = 'asc';
+        }
+    }
     public function render()
     {
-        $unidades = SistemaUnidad::where('nombre', 'like', '%' . $this->attribute . '%')
-            ->orderBy($this->sort, $this->direction)
-            ->paginate($this->pagination);
+        switch ($this->type) {
+            case 'abreviatura':
+                $unidades = SistemaUnidad::where('abreviatura', 'like', '%' . $this->attribute . '%')
+                    ->orderBy($this->sort, $this->direction)
+                    ->paginate($this->pagination);
+                break;
+            default:
+                $unidades = SistemaUnidad::where('nombre', 'like', '%' . $this->attribute . '%')
+                    ->orderBy($this->sort, $this->direction)
+                    ->paginate($this->pagination);
+        }
         return view('livewire.configuracion.sistema-unidad.lw-index', compact('unidades'));
     }
 }
