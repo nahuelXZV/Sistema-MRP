1964a71b27ff197ef5567a2c1e341a75cce045be
danic <dalbacarrasco@gmail.com>
2022-06-28 14:35:13 -0400

    Gestionar pedidos cancelados

--- app/Http/Livewire/CompraDistribucion/PedidoCancelado/LwCreate.php
+++ app/Http/Livewire/CompraDistribucion/PedidoCancelado/LwCreate.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\CompraDistribucion\PedidoCancelado;
 
+use App\Models\CompraDistribucion\Pedido;
+use App\Models\CompraDistribucion\PedidoCancelado;
 use Livewire\Component;
 
 class LwCreate extends Component
 {
+    public $pedido = [];
+
+    public function add()
+    {
+        $this->validate([
+            'pedido.pedido_id' => 'required',
+            'pedido.motivo' => 'required',
+            'pedido.descripcion' => 'required',
+            'pedido.fecha' => 'required',
+            'pedido.hora' => 'required',
+        ]);
+        $pedido = PedidoCancelado::create($this->pedido);
+        
+        return redirect()->route('pedido-cancelado.index');
+    }
+
+    public function limpiar()
+    {
+        $this->pedido = [];
+    }
+
     public function render()
     {
-        return view('livewire.compra-distribucion.pedido-cancelado.lw-create');
+        $pedidos = Pedido::all();
+        return view('livewire.compra-distribucion.pedido-cancelado.lw-create',compact('pedidos'));
     }
 }
